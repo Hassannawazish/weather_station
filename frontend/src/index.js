4a7e87d62c7198b11aa9a14acf0691ef0c5e43ef
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2023-12-26 18:24:46 +0500

    create root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App.js';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
