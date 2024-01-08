a2dae3fad5c74631f5e81862bf114dcdfeb7b39b
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2024-01-08 17:17:08 +0500

    customize navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navbar_color = {
-  backgroundColor: "lightyellow",
+  backgroundColor: "brown",
 };
 
 function Header(properties) {
   return (
     <Navbar style={navbar_color} varient="light" data-bs-theme="light">
+      <h1
+        style={{
+          color: "#C8D700",
+          textAlign: "center", // Text alignment
+          textTransform: "uppercase", // Uppercase text
+          fontSize: "2em", // Font size
+        }}
+      >
+        Weather Station Data
+      </h1>
       <Container>
         <Navbar.Brand href="/">{properties.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: "15rem", maxHeight: "7rem" }} />
       </Container>
     </Navbar>
   );
