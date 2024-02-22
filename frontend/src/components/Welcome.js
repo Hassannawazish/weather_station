cd9bd30f4475877cddfde9cad5e2a8c4fe637fd6
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2024-02-22 16:54:09 +0500

    custom Gif based weather UI

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+  integrity="sha512-qv9r6+0s1l81gsFCV4CAKt9GA4UpZ9y7tTM2aCJW0tY4cXl1g5A8R3JQ3x5Z93xcpF9U7Q3lmT7OJlT6DOsybcg=="
+  crossorigin="anonymous"
+/>
 
 const Welcome = () => (
   <div class="mt-4 p-5 bg-primary text-white rounded" >
--- frontend/src/components/weather.js
+++ frontend/src/components/weather.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../css/index.css"
+import icon from '../images/icons8-wind-turbine.gif'
+import rain from '../images/icons8-heavy-rain.gif'
 const getTemperatureColorClass = (temperature) => {
   if (temperature < 10) {
     return "blue";
@@ -14,102 +17,137 @@ const getTemperatureColorClass = (temperature) => {
 };
 
 const WeatherSection = ({ weatherData, specificData, fetchData }) => {
+  useEffect(() => {
+    // Add a class to trigger the animation after component mount
+    const cards = document.querySelectorAll(".animate-card");
+    cards.forEach((card, index) => {
+      card.style.animationDelay = `${index * 0.1}s`; // Adjust the delay based on your preference
+      card.classList.add("animate-card");
+    });
+  }, [weatherData]);
   return (
     <section>
       <div className="container">
         <div className="row">
+      
+     
           {weatherData && weatherData.data && weatherData.data.length > 0 ? (
             <>
-              {weatherData.data.map((data) => (
-                <div key={data._id} className="col-md-3">
-                  <div className="card mb-3" style={{ height: "100%" }}>
-                    <div
-                      className="card-body"
-                      style={{ backgroundColor: "#f0f8ff" }}
-                    >
-                      <h5
-                        className={`card-title temperature ${getTemperatureColorClass(
-                          data.temperature
-                        )}`}
-                      >
-                        Temperature: {data.temperature.toFixed(1)}°C
-                      </h5>
+            <div className='row'>
+            <div className="col-md-1"></div>
+                <div className="col-md-2">
+                  {weatherData.data.map((data, index) => (
+                    <div key={data._id} className="animate-card">
+                      <div className="card" style={{ height: "150px", animationDuration: "0.5s" }}>
+                        <div className="card-body" style={{ backgroundColor: "red" }}>
+                          <h5 className={`card-title temperature ${getTemperatureColorClass(data.temperature)}`}>
+                            Temperature: {data.temperature.toFixed(1)}°C
+                          </h5>
+                          <div className="animate-icon">
+                            <i className="bi bi-thermometer" style={{ fontSize: "35px" }}></i>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-              {weatherData.data.map((data) => (
-                <div key={data._id} className="col-md-3">
-                  <div className="card mb-3" style={{ height: "100%" }}>
-                    <div
-                      className="card-body"
-                      style={{ backgroundColor: "#ffe4e1" }}
-                    >
-                      {data.detail && (
-                        <>
-                          <p className="card-text">
-                            Wind Speed: {data.windspeed} m/s
-                          </p>
-                        </>
-                      )}
+                <div className="col-md-2">
+                  {weatherData.data.map((data) => (
+                    <div key={data._id} className="">
+                      <div className="card mb-2" style={{ height: "150px" }}>
+                        <div
+                          className="card-body"
+                          style={{ backgroundColor: "#ffe4e1" }}
+                        >
+                          {data.detail && (
+                            <>
+                              <p className="card-text">
+                                Wind Speed:<br/> {data.windspeed} m/s
+                              
+                              </p>
+                              <div className="animate-wind-icon">
+                                <img
+                                  src={icon} 
+                                  alt="Wind Icon"
+                                  style={{ width: "2rem", height: "2rem" }}
+                                />
+                              </div>
+                            </>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              ))}
-              {weatherData.data.map((data) => (
-                <div key={data._id} className="col-md-3">
-                  <div className="card mb-3" style={{ height: "100%" }}>
-                    <div
-                      className="card-body"
-                      style={{ backgroundColor: "#f0e68c" }}
-                    >
-                      {data.detail && (
-                        <>
-                          <p className="card-text">
-                            Pressure: {data.detail.pressure} hPa
-                          </p>
-                        </>
-                      )}
+                  ))}</div>
+                <div className="col-md-2">
+                  {weatherData.data.map((data) => (
+                    <div key={data._id} className="">
+                      <div className="card mb-2" style={{ height: "150px" }}>
+                        <div
+                          className="card-body"
+                          style={{ backgroundColor: "#f0e68c" }}
+                        >
+                          {data.detail && (
+                            <>
+                              <p className="card-text">
+                                Pressure:<br/> {data.detail.pressure} hPa
+                              </p>
+                              <i class="bi bi-moisture" style={{ fontSize: "2rem" }}></i>
+                            </>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-              {weatherData.data.map((data) => (
-                <div key={data._id} className="col-md-3">
-                  <div className="card mb-3" style={{ height: "100%" }}>
-                    <div
-                      className="card-body"
-                      style={{ backgroundColor: "#d3d3d3" }}
-                    >
-                      {data.detail && (
-                        <>
-                          <p className="card-text">
-                            Wind Direction: {data.detail.winddirection}
-                          </p>
-                        </>
-                      )}
+                <div className="col-md-2">
+                  {weatherData.data.map((data) => (
+                    <div key={data._id} className="">
+                      <div className="card mb-2" style={{ height: "150px" }}>
+                        <div
+                          className="card-body"
+                          style={{ backgroundColor: "#d3d3d3" }}
+                        >
+                          {data.detail && (
+                            <>
+                              <p className="card-text">
+                                Wind Direction:<br/> {data.detail.winddirection}
+                              </p>
+                              <i class="bi bi-arrow-up-right" style={{ fontSize: "2rem" }}></i>
+                            </>
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
-              {weatherData.data.map((data) => (
-                <div key={data._id} className="col-md-3">
-                  <div className="card mb-3" style={{ height: "100%" }}>
-                    <div
-                      className="card-body"
-                      style={{ backgroundColor: "#d3d3d3" }}
-                    >
-                      {data.detail && (
-                        <>
-                          <p className="card-text">
-                            Weather Type: {data.detail.weather_type}
-                          </p>
-                        </>
-                      )}
+                <div className="col-md-2">
+                  {weatherData.data.map((data) => (
+                    <div key={data._id} className="">
+                      <div className="card mb-2" style={{ height: "150px" }}>
+                        <div
+                          className="card-body"
+                          style={{ backgroundColor: "#d3d3d3" }}
+                        >
+                          {data.detail && (
+                            <>
+                              <p className="card-text">
+                                Weather Type: {data.detail.weather_type}
+                              </p>
+                              <img
+                                src={rain}
+                                alt="Wind Icon"
+                                style={{ width: "2rem", height: "2rem" }}
+                              />
+                            </>
+                            
+                          )}
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
-              ))}
+                <div className="col-md-1"></div>
+            </div>
             </>
           ) : (
             <p className="col">No weather data available</p>
